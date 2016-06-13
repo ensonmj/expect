fbc550303c632578844d8c9e205b92a78dfed7f9
ensonmj <ensonmj@gmail.com>
2016-06-13 17:11:59 +0800

    add win size

--- expect.go
+++ expect.go
@@ -10,6 +10,7 @@ import (
 	"regexp"
 	"syscall"
 	"unicode/utf8"
+	"unsafe"
 
 	shell "github.com/kballard/go-shellquote"
 	"github.com/kr/pty"
@@ -22,19 +23,16 @@ type buffer struct {
 }
 
 func (b *buffer) read(chunk []byte) (int, error) {
-	nread := 0
 	if b.cache.Len() > 0 {
 		n, _ := b.cache.Read(chunk)
 		if b.debug {
 			fmt.Printf("\x1b[36;1mREAD:|>%s<|\x1b[0m\r\n", string(chunk[:n]))
 			fmt.Printf("\x1b[36;1mREAD:|>%v<|\x1b[0m\r\n", chunk[:n])
 		}
-		if n == len(chunk) {
-			return n, nil
-		}
-		nread = n
+		return n, nil
 	}
-	fn, err := b.file.Read(chunk[nread:]) // this may be blocked
+
+	n, err := b.file.Read(chunk) // this may be blocked
 	if err != nil {
 		if e, ok := err.(*os.PathError); ok && e.Err == syscall.EIO {
 			// It's just the PTY telling us that it closed all good
@@ -43,19 +41,19 @@ func (b *buffer) read(chunk []byte) (int, error) {
 		}
 	}
 	if b.debug {
-		fmt.Printf("\x1b[34;1m|>%s<|\x1b[0m\r\n", string(chunk[nread:fn+nread]))
-		fmt.Printf("\x1b[34;1m|>%v<|\x1b[0m\r\n", chunk[nread:fn+nread])
-		// f, err := os.OpenFile("/tmp/expect_stream_data",
-		// 	os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-		// if err != nil {
-		// 	panic(err)
-		// }
-		// defer f.Close()
-		// if _, err = f.WriteString(string(chunk[nread : fn+nread])); err != nil {
-		// 	panic(err)
-		// }
+		fmt.Printf("\x1b[34;1m|>%s<|\x1b[0m\r\n", string(chunk[:n]))
+		fmt.Printf("\x1b[34;1m|>%v<|\x1b[0m\r\n", chunk[:n])
+		f, err := os.OpenFile("/tmp/expect_stream_data",
+			os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		if _, err = f.WriteString(string(chunk[:n])); err != nil {
+			panic(err)
+		}
 	}
-	return fn + nread, err
+	return n, err
 }
 
 func (b *buffer) unread(chunk []byte) {
@@ -80,35 +78,18 @@ func (b *buffer) unread(chunk []byte) {
 }
 
 func (b *buffer) ReadRune() (rune, int, error) {
-	l := b.cache.Len()
-
 	chunk := make([]byte, utf8.UTFMax)
-	if l > 0 {
-		n, err := b.cache.Read(chunk)
-		if err != nil {
-			return 0, 0, err
-		}
+	for n := 0; n < utf8.UTFMax; {
+		n, err := b.read(chunk[n:])
 		if utf8.FullRune(chunk[:n]) {
 			r, rL := utf8.DecodeRune(chunk)
 			if n > rL {
 				b.unread(chunk[rL:n])
 			}
-			return r, rL, nil
+			return r, rL, err
 		}
 	}
-	// else add bytes from the file, then try that
-	for l < utf8.UTFMax {
-		fn, err := b.file.Read(chunk[l : l+1])
-		if err != nil {
-			return 0, 0, err
-		}
-		l = l + fn
-		if utf8.FullRune(chunk[:l]) {
-			r, rL := utf8.DecodeRune(chunk)
-			return r, rL, nil
-		}
-	}
-	return 0, 0, errors.New("File is not a valid UTF=8 encoding")
+	return 0, 0, errors.New("file is not a valid UTF=8 encoding")
 }
 
 type ExpectSubproc struct {
@@ -116,8 +97,8 @@ type ExpectSubproc struct {
 	buf *buffer
 }
 
-func Spawn(command string) (*ExpectSubproc, error) {
-	splitArgs, err := shell.Split(command)
+func Command(cmd string) (*ExpectSubproc, error) {
+	splitArgs, err := shell.Split(cmd)
 	if err != nil {
 		return nil, err
 	}
@@ -138,17 +119,25 @@ func Spawn(command string) (*ExpectSubproc, error) {
 	}
 	proc.buf = new(buffer)
 
-	f, err := pty.Start(proc.cmd)
-	if err != nil {
-		return nil, err
-	}
-	proc.buf.file = f
-
 	return proc, nil
 }
 
-func (e *ExpectSubproc) Debug(open bool) {
-	e.buf.debug = open
+func (e *ExpectSubproc) Start() error {
+	f, err := pty.Start(e.cmd)
+	if err != nil {
+		return err
+	}
+	e.buf.file = f
+
+	return nil
+}
+
+func Spawn(cmd string) (*ExpectSubproc, error) {
+	proc, err := Command(cmd)
+	if err != nil {
+		return nil, err
+	}
+	return proc, proc.Start()
 }
 
 func (e *ExpectSubproc) Close() error {
@@ -312,4 +301,38 @@ func buildKMPTable(searchStr string) []int {
 // ExpectMatch checks whether a textual regular expression matches happened
 func (e *ExpectSubproc) ExpectMatch(regex string) (bool, error) {
 	return regexp.MatchReader(regex, e.buf)
+}
+
+func (e *ExpectSubproc) Debug(open bool) {
+	e.buf.debug = open
+}
+
+type winsize struct {
+	Height uint16
+	Width  uint16
+	xpixel uint16 //unused
+	ypixel uint16 //unused
+}
+
+func (e *ExpectSubproc) GetWinsize() (uint16, uint16, error) {
+	ws := &winsize{}
+	if _, _, errno := syscall.Syscall(syscall.SYS_IOCTL,
+		uintptr(syscall.Stdin),
+		uintptr(syscall.TIOCGWINSZ),
+		uintptr(unsafe.Pointer(ws))); errno != 0 {
+		return 0, 0, errno
+	}
+	return ws.Height, ws.Width, nil
+}
+
+func (e *ExpectSubproc) SetWinsize(height, width uint16) error {
+	ws := &winsize{Height: height, Width: width}
+	if _, _, errno := syscall.Syscall(syscall.SYS_IOCTL,
+		e.buf.file.Fd(),
+		uintptr(syscall.TIOCSWINSZ),
+		uintptr(unsafe.Pointer(ws))); errno != 0 {
+		fmt.Println(errno)
+		return errno
+	}
+	return nil
 }
